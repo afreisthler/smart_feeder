55c7d976cbc209dab74ea082138f564d5b181dc4
afreisthler <andrew.freisthler@proquest.com>
2017-07-01 23:14:37 -0500

    test commit

--- ios_src/Feeder1/FeederRunEventTableViewController.swift
+++ ios_src/Feeder1/FeederRunEventTableViewController.swift
@@ -52,15 +52,6 @@ class FeederRunEventTableViewController: UITableViewController {
         } else {
             loadSampleFeederRunEvents()
         }
-        
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
 
     }
 
